a2eef6e0a1610870e6dc153492c57b99d9dae95f
Oluwaseun Odueso <seunoduez@gmail.com>
2026-03-25 15:40:09 +0000

    chore: commit missing cors config and package-lock

---
+++ backend/config/cors.php
@@ -0,0 +1,13 @@
+<?php
+$allowed = defined('FRONTEND_URL') ? FRONTEND_URL : 'http://localhost:5173';
+
+header('Access-Control-Allow-Origin: ' . $allowed);
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Accept');
+header('Content-Type: application/json; charset=utf-8');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
